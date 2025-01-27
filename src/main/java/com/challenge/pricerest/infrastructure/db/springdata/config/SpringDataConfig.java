2c55858235506fd413c477ad5c3f76c66c94f78b
Mariano Godoy <50596171+marianogodoy82@users.noreply.github.com>
2025-01-27 13:58:08 -0300

    feat: uso simple de flyway, implementacion de jacoco, cambio version de 2 a 3 de SpringBoot

--- src/main/java/com/challenge/pricerest/infrastructure/db/springdata/config/SpringDataConfig.java
+++ src/main/java/com/challenge/pricerest/infrastructure/db/springdata/config/SpringDataConfig.java
@@ -3,21 +3,17 @@ package com.challenge.pricerest.infrastructure.db.springdata.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.challenge.pricerest.infrastructure.db.springdata.repository")
 @ConfigurationProperties("spring.datasource")
-@Slf4j
-@NoArgsConstructor
-@Data
 @EnableJpaAuditing
 @EntityScan(basePackages = "com.challenge.pricerest.infrastructure.db.springdata.dbo")
+@DependsOn("flyway")
 public class SpringDataConfig
 {
 
